33819583655093e0c4584ae958cd351bf4a7af19
hamed <hamedremy @hotmail.fr>
2022-04-07 19:34:32 +0200

    add co bdd

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("ok");
+console.log("ok");import {config} from "dotenv";
+config();
+
+import express from "express";
+import mongoose, {Mongoose} from "mongoose";
+
+async function startServer(): Promise<void> {
+
+    const m: Mongoose = await mongoose.connect(process.env.MONGO_URI as string, {
+        auth: {
+            username: process.env.MONGO_USER  as string,
+            password: process.env.MONGO_PASSWORD as string
+        }
+    });
+    const app = express();
+    app.listen(process.env.PORT, function() {
+        console.log("Server listening on port " + process.env.PORT);
+    });
+}
+
+startServer().catch(console.error);
