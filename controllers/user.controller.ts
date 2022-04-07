93eaa784c5406f70d98bbcf05d6062164cd47750
hamed <hamedremy @hotmail.fr>
2022-04-07 20:02:17 +0200

    schema user + class userController did

---
+++ controllers/user.controller.ts
@@ -0,0 +1,31 @@
+import express, {Router, Request, Response} from "express";
+
+
+export class UserController {
+
+    async createUser(req: Request, res: Response) {
+        const userBody = req.body;
+        if(!UserBody.name || !userBody.intensity || !userBody.price) {
+            res.status(400).end(); // 400 -> bad request
+            return;
+        }
+        try {
+            const user = await UserService.getInstance().createUser({
+                firstName: userBody.firstName,
+                lastName: userBody.lastName,
+                mail: userBody.mail,
+                password: userBody.password
+            });
+            res.json(user);
+        } catch(err) {
+            res.status(400).end(); // erreur des données utilisateurs
+            return;
+        }
+    }
+
+    buildRoutes(): Router {
+        const router = express.Router();
+        router.post('/', express.json(), this.createUser.bind(this)); // permet de forcer le this lors de l'appel de la fonction sayHello
+        return router;
+    }
+}
---
+++ model/user.model.ts
@@ -0,0 +1,35 @@
+import mongoose, {Schema, Document, Model} from "mongoose";
+
+const userSchema = new Schema({
+    firstName: {
+        type: Schema.Types.String,
+        required: true
+    },
+    lastName: {
+        type: Schema.Types.String,
+        required: true
+    },
+    mail: {
+        type: Schema.Types.String,
+        required: true
+    },
+    password: {
+        type: Schema.Types.String,
+        required: true
+    }
+}, {
+    collection: "users",
+    timestamps: true,
+    versionKey: false
+});
+
+export interface UserProps {
+    firstName: string,
+    lastName: string,
+    mail: string,
+    password: string
+}
+
+export type UserDocument = UserProps & Document;
+
+export const CoffeeModel: Model<UserDocument> = mongoose.model<UserDocument>("User", userSchema);
