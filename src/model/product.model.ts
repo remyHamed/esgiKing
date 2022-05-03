0fe5c0d9aafb3ed5844222289e25a7b34fd43f74
Enzo SOARES <enzoayrsoares@gmail.com>
2022-05-03 17:48:41 +0100

    Creation of ingredient and product models

---
+++ src/model/product.model.ts
@@ -0,0 +1,20 @@
+import mongoose, {Schema, Document, Model} from "mongoose";
+
+const productSchema = new Schema({
+    name: {
+        type: Schema.Types.String,
+        required: true
+    },
+    ingredients: {
+        type: [Schema.Types.String],
+        required: true
+    }
+})
+
+export interface ProductProps {
+    name: string,
+    ingredients: string[]
+}
+
+export type ProductDocument = ProductProps & Document;
+export const ProductModel: Model<ProductDocument> = mongoose.model<ProductDocument>("Ingredient", productSchema);
